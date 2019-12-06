9ee08d0230dc89845fd721436fcc5d2ba3dc89c6
Angelo Otero <otero.angelo20@gmail.com>
2019-12-06 13:07:05 -0500

    initial commit

---
+++ src/Artists.js
@@ -0,0 +1,52 @@
+//jshint esversion: 6
+
+const Artists = [{
+  artistImg: "",
+  name: "",
+  years: "",
+  artMovement: "",
+  bio: "",
+  topWorks: [
+    {
+      workImg: "",
+      title:"",
+      year: "",
+    },
+    {
+      workImg: "",
+      title:"",
+      year: "",
+    },
+    {
+      workImg: "",
+      title:"",
+      year: "",
+    }
+  ]
+},
+{
+  artistImg: "",
+  name: "",
+  years: "",
+  artMovement: "",
+  bio: "",
+  topWorks: [
+    {
+      workImg: "",
+      title:"",
+      year: "",
+    },
+    {
+      workImg: "",
+      title:"",
+      year: "",
+    },
+    {
+      workImg: "",
+      title:"",
+      year: "",
+    }
+  ]
+}];
+
+export default Artists;
---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+//jshint esversion: 6
+
+import React from "react";
+
+function App () {
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+}

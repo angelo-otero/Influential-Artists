f34ab6597e022b61de41ec42fcf7758540a7cea5
Angelo Otero <otero.angelo20@gmail.com>
2020-12-16 13:45:34 -0500

    Updates

--- docs/Clicked Artists.js
+++
@@ -1,6 +0,0 @@
-//need to be a separate file so this array won't reset with the state changes
-
-let artistArr = [];
-
-export default artistArr;
-
--- docs/index.js
+++
@@ -1,7 +0,0 @@
-//jshint esversion: 6
-
-import React from "react";
-import ReactDom from "react-dom";
-import App from "./components/App.jsx"
-
-ReactDom.render(<App />, document.getElementById("root"));

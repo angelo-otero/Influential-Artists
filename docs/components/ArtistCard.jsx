c5ecb3a95ea4af66fe4b717019e34220382d9b1f
Angelo Otero <otero.angelo20@gmail.com>
2020-12-18 14:06:57 -0500

    Add files via upload

---
+++ docs/components/ArtistCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+
+function ArtistCard (props) {
+  
+
+  function handleClick () {
+    props.handleClickedArtist(props.name);
+  }
+
+  return(
+    <div className="artist-card" onClick = {handleClick}>
+      <div className="artist-image">
+        <img src={props.artistImg} alt="artist profile"/>
+      </div>
+      
+      <div className="artist-info">
+        <h3 className="artist-name">{props.name}</h3>
+      </div>
+  
+    </div>
+  );
+}
+
+export default ArtistCard;
